4509602dee39975b5bf9295eb42c00184a3ba6d0
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-02-22 22:48:21 +0100

    Zirkuluak txikiak direnean etiketak ezkutatu eta handitzean bistaratu

--- js/erraustegiak.js
+++ js/erraustegiak.js
@@ -1,34 +1,76 @@
-var Erraustegia = function(mapa, datuak) {
+var Erraustegia = function(mapa, erraustegia, zirkuluak) {
     "use strict";
-
-    this.mapa = mapa;
-
-    this.izena = datuak.izena;
-    this.koordenatuak = datuak.koordenatuak;
-
-    this.zirkuluak = [];
-    this.etiketak = [];
-};
-
-Erraustegia.prototype.marraztuZirkuluak = function(zirkuluak) {
 
     var self = this;
 
-    zirkuluak.forEach(function(element, index, array) {
+    this.mapa = mapa;
 
-        self.zirkuluak.push(L.circle(self.koordenatuak, element.distantzia, {
+    this.izena = erraustegia.izena;
+    this.koordenatuak = erraustegia.koordenatuak;
+    this.zirkuluak = zirkuluak;
+
+    this.leaflet_zirkuluak = [];
+    this.etiketak = [];
+
+    this.mapa.on('zoomend', function() {
+
+        // Zirkuluak txikiak direnean etiketak ezkutatu, bestela elkarren gainean ikusten dira.
+        if (mapa.getZoom() < 11) {
+
+            if (self.etiketakIkusgai()) {
+
+                self.ezabatuEtiketak();
+
+            }
+        } else {
+
+            if (self.zirkuluakIkusgai() && !self.etiketakIkusgai()) {
+
+                self.gehituEtiketak();
+
+            }
+        }
+    });
+};
+
+Erraustegia.prototype.marraztuZirkuluak = function() {
+
+    var self = this;
+
+    self.zirkuluak.forEach(function(element, index, array) {
+
+        self.leaflet_zirkuluak.push(L.circle(self.koordenatuak, element.distantzia, {
             color: 'red',
             fillColor: '#f03',
             fillOpacity: element.opakotasuna
         }).addTo(self.mapa));
 
-        var etiketa = new L.Label();
-        etiketa.setContent(element.distantzia / 1000 + " km");
-        etiketa.setLatLng([self.zirkuluak[index].getBounds().getCenter().lat, self.zirkuluak[index].getBounds().getEast()]);
+        self.gehituEtiketa(self.leaflet_zirkuluak[index].getBounds().getCenter().lat, self.leaflet_zirkuluak[index].getBounds().getEast(), element.distantzia);
 
-        self.mapa.showLabel(etiketa);
-        self.etiketak.push(etiketa);
     });
+
+};
+
+Erraustegia.prototype.gehituEtiketak = function() {
+
+    var self = this;
+
+    self.zirkuluak.forEach(function(element, index, array) {
+
+        self.gehituEtiketa(self.leaflet_zirkuluak[index].getBounds().getCenter().lat, self.leaflet_zirkuluak[index].getBounds().getEast(), element.distantzia);
+
+    });
+};
+
+Erraustegia.prototype.gehituEtiketa = function(lat, lng, distantzia) {
+
+    var etiketa = new L.Label();
+
+    etiketa.setContent(distantzia / 1000 + " km");
+    etiketa.setLatLng([lat, lng]);
+
+    this.mapa.showLabel(etiketa);
+    this.etiketak.push(etiketa);
 
 };
 
@@ -36,16 +78,39 @@ Erraustegia.prototype.ezabatuZirkuluak = function() {
 
     var self = this;
 
-    self.zirkuluak.forEach(function(element, index, array) {
+    self.leaflet_zirkuluak.forEach(function(element, index, array) {
 
         self.mapa.removeLayer(element);
 
     });
+
+    self.zirkuluak = [];
+
+    self.ezabatuEtiketak();
+
+};
+Erraustegia.prototype.ezabatuEtiketak = function () {
+
+    var self = this;
 
     self.etiketak.forEach(function(element, index, array) {
 
         self.mapa.removeLayer(element);
 
     });
+
+    self.etiketak = [];
+
+};
+
+Erraustegia.prototype.zirkuluakIkusgai = function () {
+
+    return this.leaflet_zirkuluak.length > 0;
+
+};
+
+Erraustegia.prototype.etiketakIkusgai = function () {
+
+    return this.etiketak.length > 0;
 
 };
